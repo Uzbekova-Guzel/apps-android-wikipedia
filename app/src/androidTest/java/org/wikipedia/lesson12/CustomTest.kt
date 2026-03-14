cfe06a4841a77c4a533690f6d8b3d566caf757fb
guzeluzbekova <guzeluz.qa@gmail.com>
2026-03-14 15:34:21 +0100

    Created lesson12

---
+++ app/src/androidTest/java/org/wikipedia/lesson12/CustomTest.kt
@@ -0,0 +1,34 @@
+package org.wikipedia.lesson12
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.kaspersky.kaspresso.kaspresso.Kaspresso
+import com.kaspersky.kaspresso.params.FlakySafetyParams
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import org.junit.Rule
+import org.junit.Test
+import org.wikipedia.lesson08.OnboardingScreen
+import org.wikipedia.main.MainActivity
+
+//https://kasperskylab.github.io/Kaspresso/ru/Wiki/Kaspresso_configuration/
+abstract class CustomTest(
+    kaspressoBuilder: Kaspresso.Builder = Kaspresso.Builder.simple {
+        flakySafetyParams = FlakySafetyParams.custom(30000)
+    }
+) : TestCase(kaspressoBuilder) {
+
+    @get:Rule
+    val testRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun verifyConfig() {
+        run {
+            OnboardingScreen.skipButton.hasText("Continue")
+        }
+    }
+}
+
+class ConfigurationTest : CustomTest(
+    Kaspresso.Builder.simple {
+        flakySafetyParams = FlakySafetyParams.custom(80000)
+    }
+)
