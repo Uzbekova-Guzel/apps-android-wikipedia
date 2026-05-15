4ca5cfcd42cc9c0d684011002bb3454069aeea58
guzeluzbekova <guzeluz.qa@gmail.com>
2026-05-15 19:33:38 +0200

    Added lesson28

--- app/src/androidTest/java/org/wikipedia/lesson28/BuildConfigExamples.kt
+++ app/src/androidTest/java/org/wikipedia/lesson28/BuildConfigExamples.kt
@@ -1,9 +1,10 @@
-package org.wikipedia.lesson24.homework
+package org.wikipedia.lesson28
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import io.github.kakaocup.compose.rule.KakaoComposeTestRule
 import org.junit.Rule
 import org.junit.Test
+import org.wikipedia.BuildConfig
 import org.wikipedia.lesson18.homework.screens.onboarding.OnboardingScreen
 import org.wikipedia.lesson18.homework.screens.onboarding.WikipediaLanguagesScreen
 import org.wikipedia.lesson19.homework.extentions.action
@@ -12,7 +13,9 @@ import org.wikipedia.lesson19.homework.utils.BaseTest
 import org.wikipedia.lesson24.screens.AddLanguageScreen
 import org.wikipedia.main.MainActivity
 
-class ComposeExampleTests : BaseTest() {
+val testValue = BuildConfig.DEFAULT_RESTBASE_URI_FORMAT
+
+class ExampleTests : BaseTest() {
 
     @get:Rule(order = 1)
     val composeTestRule = createAndroidComposeRule<MainActivity>()
@@ -31,16 +34,16 @@ class ComposeExampleTests : BaseTest() {
                 action.click(addLanguageButton)
             }
             AddLanguageScreen {
-                itemWithText("Español") {
-                    verify.assertTrimmedTextIsEquals(canonicalName, "Spanish")
-                    verify.assertTrimmedTextIsEquals(localizedName, "Español")
+                itemWithText(BuildConfig.LOCALIZED_LANGUAGE) {
+                    verify.assertTrimmedTextIsEquals(canonicalName, BuildConfig.CANONICAL_LANGUAGE)
+                    verify.assertTrimmedTextIsEquals(localizedName, BuildConfig.LOCALIZED_LANGUAGE)
                     action.clickIfEnabled(canonicalName)
                 }
             }
             action.click(WikipediaLanguagesScreen.backButton)
             OnboardingScreen.page(0) {
                 languagesByIndex(1) {
-                    verify.containsText(this, "Español")
+                    verify.containsText(this, BuildConfig.LOCALIZED_LANGUAGE)
                 }
             }
         }
